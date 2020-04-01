a389122abb0b23261f8e2b092f5af52ecf3ca005
Rochellezsy <zhoushiyingzzz@gmail.com>
2020-04-01 13:04:17 +0800

    Restful api use test.

--- main.go
+++ main.go
@@ -1,22 +1,46 @@
 package main
 
 import (
-	"fmt"
-	"io/ioutil"
+	"github.com/gin-gonic/gin"
 	"net/http"
 )
 
-func sayHello(w http.ResponseWriter, r *http.Request){
-	b, _ := ioutil.ReadFile("./hello.txt")
-	_, _ = fmt.Fprintln(w,string(b))
+func sayHello(c *gin.Context) {
+	c.JSON(200, gin.H{
+		"message": "Hello Rochelle!",
+	})
 }
 
 func main() {
-	http.HandleFunc("/hello", sayHello)
-	err := http.ListenAndServe(":9090",nil)
-	if err != nil {
-		fmt.Printf("http server failed, err:%v\n", err)
-		return
-	}
-	
+	r := gin.Default() //返回默认的路由引擎
+
+	//指定用户使用GET请求访问/hello，执行sayHello函数
+	r.GET("/hello", sayHello)
+
+	r.GET("/book", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"method": "GET",
+		})
+	})
+
+	r.POST("/book", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"method": "POST",
+		})
+	})
+
+	r.PUT("/book", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"method": "PUT",
+		})
+	})
+
+	r.DELETE("/book", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"method": "DELETE",
+		})
+	})
+
+	// 启动服务
+	r.Run()
 }
